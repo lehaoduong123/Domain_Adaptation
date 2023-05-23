f146c585966addec7619f1c4370a9201dde323ff
Oliver Le <lehaoduong.vn@gmail.com>
2023-05-23 12:45:52 +0700

    first_draft

--- dataset.py
+++ dataset.py
@@ -6,30 +6,67 @@ import torch
 import copy
 from PIL import Image
 from glob import glob
+import json
 
 
 def get_transform(*args, **kwargs):
-    ...
-    # TODO
-    # perform data augmentation
-    # convert data type and value range
-    # return transform
+    # Define the transformations to be applied
+    if kwargs['train']:
+        transforms = [
+            T.Resize((800, 800)),
+            T.RandomHorizontalFlip(0.5),
+            T.RandomVerticalFlip(0.5),
+            T.RandomRotation(30),
+            T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+            T.RandomPerspective(),
+            T.RandomAffine(30),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
+    else:
+        transform = [
+        T.Resize((800, 800)),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
+    # Combine the transformations into a single transform
+    transform = T.Compose(transforms)
+    
+    return transform
 
 
 class SourceDataset(Dataset):
 
     def __init__(self, root: str, split: str = "train", transform=None, *args, **kwargs) -> None:
         super().__init__()
-        self.coco = COCO(...)
-        # TODO
+
+        self.root = root
+        self.split = split
+        self.transform = transform
+
+        ann_file = os.path.join(self.root, f"org/{self.split}.coco.json")
+        with open(ann_file, "r") as f:
+            self.coco = json.load(f)
+        
+        self.ids = list(sorted(self.coco["images"].keys()))
 
     def _load_image(self, index: int):
         ...
+        img_info = self.coco["images"][self.ids[index]]
+        ima_path = os.path.join(self.root, img_info["file_name"])
+        img = Image.open(ima_path).convert("RGB")
+        return img
         # return image
 
     def _load_target(self, index: int):
-        target = self.coco.loadAnns(self.coco.getAnnIds(index))
-        ...
+        img_id = self.coco["images"][self.ids[index]]
+        anns = self.coco.loadAnns(self.coco.getAnnIds(img_id))
+
+        boxes = [ann["bbox"] + [ann["category_id"]] for ann in anns]
+
+        target = {}
+        target["boxes"] = torch.tensor(boxes, dtype=torch.float32)
+        target["labels"] = torch.tensor([ann["category_id"] for ann in anns], dtype=torch.int64)
+
+        return target
         # return target
 
     def __getitem__(self, index: int):
@@ -62,27 +99,82 @@ class SourceDataset(Dataset):
         targ["iscrowd"] = torch.tensor([t["iscrowd"]  for t in target], dtype=torch.int64)
         
         # TODO: make sure your image is scaled properly
+        return image, targ
         # return image and target
 
     def __len__(self) -> int:
-        ...
-        # return the length of dataset
+        return len(self.ids)
 
 
 class TargetDataset(Dataset):
 
     def __init__(self, root: str, split: str = "train", transform=None, *args, **kwargs) -> None:
         super().__init__()
-        # TODO
+        self.root = root
+        self.split = split
+        self.transform = transform
+
+        # Load the annotations for the validation split of the target dataset
+        if self.split == "val":
+            ann_file = os.path.join(self.root, f"fog/{self.split}.coco.json")
+            with open(ann_file, "r") as f:
+                self.coco = json.load(f)
+            self.ids = list(sorted(self.coco["images"].keys()))
+        else:
+            self.ids = self._load_image_ids(root)
+
+        
+    def _load_image_ids(self, root: str):
+        # Load the image ids for the training split of the target dataset
+        image_ids = []
+        for img_path in glob(os.path.join(root, f"fog/{self.split}*.jpg")):
+            img_id = os.path.splitext(os.path.basename(img_path))[0]
+            image_ids.append(img_id)
+        return image_ids
 
     def _load_image(self, index: int):
-        ...
-        # return image
+        # Load the image for the specified index
+        img_id = self.ids[index]
+        img_path = os.path.join(self.root, self.split, f"{img_id}.jpg")
+        img = Image.open(img_path).convert("RGB")
+        return img
+
+    def _load_target(self, index: int):
+        # Load the annotations for the specified index
+        if self.split == "train":
+            # During training, return an empty dictionary for the target
+            return {}
+
+        elif self.split == "val":
+            # During validation, load the annotations for the specified index
+            img_id = self.ids[index]
+            ann_ids = self.annotations.getAnnIds(imgIds=img_id)
+            anns = self.annotations.loadAnns(ann_ids)
+
+            # Convert the bounding boxes to the required format
+            boxes = [ann["bbox"] + [ann["category_id"]] for ann in anns]
+
+            # Create the target dictionary
+            target = {}
+            target["boxes"] = torch.tensor(boxes, dtype=torch.float32)
+            target["labels"] = torch.tensor([ann["category_id"] for ann in anns], dtype=torch.int64)
+            target["image_id"] = torch.tensor([img_id], dtype=torch.int64)
+            target["area"] = torch.tensor([ann["area"] for ann in anns], dtype=torch.float32)
+            target["iscrowd"] = torch.tensor([ann["iscrowd"] for ann in anns], dtype=torch.int64)
+
+            return target
 
     def __getitem__(self, index: int):
-        ...
-        # return image and target
+        # Load the image and target for the specified index
+        image = self._load_image(index)
+        target = self._load_target(index)
+
+        # Apply the specified transformations to the image and target
+        if self.transform is not None:
+            image, target = self.transform(image, target)
+
+        return image, target
 
     def __len__(self) -> int:
-        ...
-        # return the length of dataset
+        # Return the length of the dataset
+        return len(self.ids)
--- train.py
+++ train.py
@@ -7,6 +7,8 @@ import logging
 from pycocotools.coco import COCO
 from model import DA_model
 import torchvision
+import matplotlib.pyplot as plt
+from dataset import SourceDataset, TargetDataset, get_transform
 
 
 def train_one_epoch(model, optimizer, source_loader, target_loader, epoch):
@@ -30,6 +32,28 @@ def train_one_epoch(model, optimizer, source_loader, target_loader, epoch):
         scaler.update()
 
         pbar.set_postfix({'loss': losses.item()})
+    
+def normal_train_one_epoch(model, optimizer, source_loader, epoch):
+    model.train()
+
+    pbar = tqdm(source_loader)
+    total_loss = 0.0
+    num_batches = len(source_loader)
+    for i, (images, targets) in enumerate(pbar):
+        images = list(image.to('cuda', non_blocking=True) for image in images)
+        targets = [{k: v.to('cuda', non_blocking=True) for k, v in t.items()} for t in targets]
+
+        optimizer.zero_grad()
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+        losses.backward()
+        optimizer.step()
+
+        total_loss += losses.item()
+        pbar.set_postfix({'loss': losses.item()})
+    
+    avg_loss = total_loss / num_batches
+    print(f"Epoch {epoch}: Average Loss = {avg_loss:.4f}")
 
 
 @torch.no_grad()
@@ -40,7 +64,7 @@ def validation(model, data_loader):
     for images, targets in tqdm(data_loader):
         images = list(image.to('cuda', non_blocking=True) for image in images)
         predictions = model(images)
-        predictions = ...  # postprocess: modify format to meet metric's requirements
+        predictions = [{k: v.to('cpu') for k, v in t.items()} for t in predictions]
         metric.update(predictions, targets)
     
     result = metric.compute()
@@ -53,14 +77,17 @@ def build_dataloader():
         ...
         # TODO
         # depends on your code
-        # return tuple(zip(*batch))
+        return tuple(zip(*batch))
 
     # TODO
     from dataset import SourceDataset, TargetDataset, get_transform
-    source_dataset = SourceDataset()
-    target_dataset = TargetDataset()
-    val_dataset = ...
-
+    source_dataset = SourceDataset(root="content/hw3_dataset/", split="train", transforms=get_transform("train"))
+    target_dataset = TargetDataset(root="content/hw3_dataset/", split="train", transforms=get_transform("train"))
+    val_dataset =   TargetDataset(root="content/hw3_dataset/", split="val", transforms=get_transform("val"))
+    
+    batch_size = 4
+    num_workers = 4
+    
     source_loader = torch.utils.data.DataLoader(source_dataset, batch_size, num_workers, shuffle=True, collate_fn=collate_fn)
     target_loader = torch.utils.data.DataLoader(target_dataset, batch_size, num_workers, shuffle=True, collate_fn=collate_fn)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size, num_workers, shuffle=True, collate_fn=collate_fn)
@@ -68,19 +95,26 @@ def build_dataloader():
     return source_loader, target_loader, val_loader
 
 
-model = DA_model(n_classes, load_source_model=False)
+model = DA_model(9, load_source_model=False)
 model = model.to('cuda')
 
 
 # TODO
-optimizer = ...
-scheduler = ...
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 num_epochs = 50
+DA = False
 
 best_epoch = 0
-source_loader, target_loader, val_loader = build_dataloader()
-best_map = map_50 = validation(model, val_loader)
+mAP50_values = []
+epoch_numbers = []
+# if(DA):
 for epoch in range(num_epochs):
+#         source_loader = SourceDataset(root="content/hw3_dataset/", split="train", transforms=get_transform("train"))
+#         train_one_epoch(model, optimizer, source_loader, epoch)
+#     else:
+    source_loader, target_loader, val_loader = build_dataloader()
+    best_map = map_50 = validation(model, val_loader)
     train_one_epoch(model, optimizer, source_loader, target_loader, epoch)
     scheduler.step()
 
@@ -88,4 +122,14 @@ for epoch in range(num_epochs):
     if map_50 > best_map:
         best_map = map_50
         best_epoch = epoch
-        torch.save(model.state_dict(), ...)
+        torch.save(model.state_dict(), f'model_{epoch}.pth')
+    if epoch % 10 ==0:
+        mAP50_values.append(map_50)
+        epoch_numbers.append(epoch)
+
+plt.plot(epoch_numbers, mAP50_values)
+plt.xlabel('epoch')
+plt.ylabel('map_50')
+plt.title("mAP@50 vs Epoch")
+plt.show()
+
